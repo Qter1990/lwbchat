57dae4c469efa0f74e0d1eb32bfc453f34f83e66
林文斌 <mac@macdemacbook-pro.local>
2016-02-20 15:53:28 +0800

    Initial Commit

---
+++ lwbchat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lwbchat
+//
+//  Created by Mac on 16/2/20.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lwbchat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lwbchat
+//
+//  Created by Mac on 16/2/20.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
